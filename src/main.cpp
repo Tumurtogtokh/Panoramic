eb57e2895c1aeb79bdbea6f92f28bbabe6d9f9fd
tumurtogtokh <tdavaakhuu1@sheffield.ac.uk>
2019-09-16 13:30:42 +0100

    concurrency done

--- src/main.cpp
+++ src/main.cpp
@@ -51,60 +51,50 @@ int main(int argc, char *argv[])
         std::cout << d << std::endl;
     std::cout << "-------------------------------------------------------" << std::endl;
 
-    // Start image processing
-    std::string curr_input;
+    // Checking available cores
     unsigned int nCores = std::thread::hardware_concurrency();
     std::cout << "This machine supports concurrency with " << nCores << " cores available" << std::endl;
 
-    for (auto dir : directories)
+    // Start image processing
+    std::string curr_input;
+    std::thread runners[directories.size()];
+
+    for (size_t i = 0; i < directories.size(); i++)
     {
         // Loads names of image files
-        curr_input = Utils::get_absolute_path(std::string(input_dir + dir).c_str());
+        curr_input = Utils::get_absolute_path(std::string(input_dir + directories.at(i)).c_str());
         std::cout << "\nCurr dir: " << curr_input << std::endl;
         Utils::stringvec files = Utils::load_image_names(curr_input.c_str());
+        std::string dir = directories.at(i);
 
-        // Initialise image processor
-        auto thread_id = std::this_thread::get_id();
-        std::stringstream ss;
-        ss << thread_id;
+        // starting a thread with lambda
+        auto run_processor = [dir, curr_input, files]() {
+            // Initialise image processor
+            auto thread_id = std::this_thread::get_id();
+            std::stringstream ss;
+            ss << thread_id;
+            Processor proc("Img Processor thread - " + ss.str(), output);
+            std::cout << proc.Name() << std::endl;
 
-        Processor proc("Img Processor thread - " + ss.str(), output);
+            // Adding images to Processor
+            for (size_t i = 0; i < files.size(); i++)
+            {
+                Image img((curr_input + "/" + files.at(i)).c_str());
+                proc.AddImage(img);
+            }
 
-        // Adding images to Processor
-        for (size_t i = 0; i < files.size(); i++)
-        {
-            Image img((curr_input + "/" + files.at(i)).c_str());
-            proc.AddImage(img);
-        }
-        // Making Panorama
-        proc.MakePanorama(dir + ".jpg");
-        proc.ClearImages();
+            // Making Panorama
+            proc.MakePanorama(dir + ".jpg");
+        };
+
+        runners[i] = std::thread(run_processor);
     }
 
-    // Making Panorama
-    // std::cout << "Image Processor: " << proc.Name() << std::endl;
-    // proc.MakePanorama("test_pano.jpg");
-
-    // Loading image names from each directory and printing it
-    // stringvec img_names;
-    // for (auto dir : dirs)
-    // {
-    //     std::cout << "Reading from: " << get_absolute_path(std::string(input_dir + dir).c_str()) << std::endl;
-    //     read_files_names(std::string(input_dir + dir).c_str(), img_names);
-
-    //     for (auto i : img_names)
-    //         std::cout << i << std::endl;
-
-    //     std::cout << "-------------------------------------------------------" << std::endl;
-    //     img_names.clear();
-    // }
-
-    // Image image2(image);
-    // proc.ApplyMedianBlur(image, image2, 37);
-
-    // cv::Size ksize(11,11);
-    // Image processed = proc.ApplyGaussianBlur(image, ksize);
-    // proc.DisplayImages(image, image2);
+    //Join the threads with the main thread
+    for (size_t i = 0; i < directories.size(); ++i)
+    {
+        runners[i].join();
+    }
 
     exit(0);
 }
